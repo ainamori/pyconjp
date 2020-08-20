c15c6f57aaed62d2f5a07750c087787af366e7a9
Akira Inamori <ainamori@iij.ad.jp>
2020-08-20 16:38:08 +0900

    mone class method demo

--- src/demo.py
+++ src/demo.py
@@ -24,3 +24,13 @@ class DemoClass:
         json_body = json.dumps(messages)
         status_code = 200
         return json_body, status_code
+
+    # @classmethod
+    def none_class_get(self, demo_path):
+        messages = {
+            "your_path": demo_path,
+            "messages": "Static Message"
+        }
+        json_body = json.dumps(messages)
+        status_code = 200
+        return json_body, status_code
